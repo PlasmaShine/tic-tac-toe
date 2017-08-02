a87fef3d46614645f0229cace43ec52fd537ae4a
James Ball <jamesball@Jamess-MacBook-Pro.local>
2017-08-02 17:59:47 +0900

    Add 'turn' method

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,42 @@
-class GenerateBoard():
+class BoardObject:
 
     def __init__(self, rows, columns):
-        self.board = list()
         self.rows = int(rows)
         self.columns = int(columns)
 
     def generate_board(self):
+        board = list()
         for row in range(self.rows):
-            self.board.append(list())
+            board.append(list())
             for column in range(self.columns):
-                self.board[row].append('')
-        return self.board
+                board[row].append('')
+        return board
+
+class Board(BoardObject):
+
+    def __init__(self):
+        super().__init__(3, 3)
+
+    def print_board(self):
+        board = self.generate_board()
+        for row in board:
+            print(' '.join(row))
+
+player = 1
+def turn(board, player):
+    while True:
+        try:
+            tile = input('{0} Turn: '.format(player))
+            row = int(tile[0])
+            column = int(tile[-1])
+            if all(iterable in range(3) for iterable in (row, column,)):
+                if board[row][column] == '':
+                    board[row][column] = player
+                    break
+                else:
+                    print('Invalid turn')
+            else:
+                print('Invalid input')
+        except ValueError:
+            print('Invalid input')
+    return board
